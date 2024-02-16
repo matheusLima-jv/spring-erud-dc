7b58580044ce4dec56f22a1e78f9e17eadbcb538
MattDev2024 <matheus.ram.lima@gmail.com>
2024-02-16 02:28:47 -0300

    implementado remoção do JWconfigurer, altereção para securityconfig diretamente

--- src/main/java/br/nbb/Application.java
+++ src/main/java/br/nbb/Application.java
@@ -1,13 +1,32 @@
 package br.nbb;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder.SecretKeyFactoryAlgorithm;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+		
+		Map<String, PasswordEncoder> encoders = new HashMap<>();
+		var pbkd = new Pbkdf2PasswordEncoder("",8,185000,SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256);
+		encoders.put("pbkdf2",pbkd );
+		DelegatingPasswordEncoder passwordEncoder = new DelegatingPasswordEncoder("pbkdf2", encoders);
+		passwordEncoder.setDefaultPasswordEncoderForMatches(pbkd);
+
+		
+		String pssw = passwordEncoder.encode("admin1235");
+		System.out.println("hasjh --->> " + pssw);
+	 		
+		
 	}
 
 }
--- src/main/java/br/nbb/security/jwt/JwtConfigurer.java
+++ src/main/java/br/nbb/security/jwt/JwtConfigurer.java
@@ -1,25 +1,27 @@
 package br.nbb.security.jwt;
-
+// OBJETO DESNECESSARIO 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
+// OBJETO DESNECESSARIO 
 public class JwtConfigurer extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity>{
-	
+// OBJETO DESNECESSARIO 
 	@Autowired
 	private JwtTokenProvider tokenProvider;
-	
+	// OBJETO DESNECESSARIO 
 	public JwtConfigurer(JwtTokenProvider tokenProvider) {
 		this.tokenProvider = tokenProvider;
 	}
-
+	// OBJETO DESNECESSARIO 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		JwtTokenFilter customFilter = new JwtTokenFilter(tokenProvider);
 		http.addFilterBefore(customFilter, UsernamePasswordAuthenticationFilter.class);
 	}
+	
+	// OBJETO DESNECESSARIO 
 
 	
 }
